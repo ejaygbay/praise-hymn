562064969fe3468e193e1a076429413fb046b0a0
Emmanuel Jaygbay <emmanueljaygbay@gmail.com>
2022-05-29 09:30:33 +0000

    hide and show play and stop icons implemented.

--- www/assets/js/extended/home.js
+++ www/assets/js/extended/home.js
@@ -18,13 +18,13 @@ const loadSongs = (id) => {
                     </div>
                 </div>
                 <div class="action-sec">
-                    <div>
-                        <ion-icon class="play-icon action-icons" name="play"></ion-icon>
-                        <ion-icon class="stop-icon action-icons" name="stop"></ion-icon>
+                    <div id="${song_title}_parent" class="" onclick="actionButtonsHandler(event)">
+                        <ion-icon id="${song_title}_play" class="play-icon action-icons" name="play"></ion-icon>
+                        <ion-icon id="${song_title}_stop" class="stop-icon action-icons" name="stop"></ion-icon>
                     </div>
                     <div>
-                        <ion-icon class="unlike-icon action-icons" name="heart-outline"></ion-icon>
-                        <ion-icon class="like-icon action-icons" name="heart"></ion-icon>
+                        <ion-icon id="${song_title}_unlike" class="unlike-icon action-icons" name="heart-outline"></ion-icon>
+                        <ion-icon id="${song_title}_like" class="like-icon action-icons" name="heart"></ion-icon>
                     </div>
                 </div>
             </article>`;
@@ -39,7 +39,6 @@ const loadSongs = (id) => {
         addEventToUnlikeIcon('songs-section');
     }
 }
-
 
 const openFullSong = (id) => {
     console.log(id);
@@ -77,33 +76,49 @@ const addEventToUnlikeIcon = (section) => {
 }
 
 const addEventToLikeIcon = (section) => {
-        document.querySelectorAll(".like").forEach(ele => {
-            ele.addEventListener("click", e => {
-                let parent_class = e.target.parentElement.className;
-                let splited_parent_class = parent_class.split("-")[0].trim();
-                songs[splited_parent_class].like = false;
-                hideIcon(`.${section} .${parent_class} .like`);
-                showIcon(`.${section} .${parent_class} .unlike`);
+    document.querySelectorAll(".like").forEach(ele => {
+        ele.addEventListener("click", e => {
+            let parent_class = e.target.parentElement.className;
+            let splited_parent_class = parent_class.split("-")[0].trim();
+            songs[splited_parent_class].like = false;
+            hideIcon(`.${section} .${parent_class} .like`);
+            showIcon(`.${section} .${parent_class} .unlike`);
 
-                if (section === "favorites") {
-                    let fav_play = document.querySelector(`.${section} .${splited_parent_class} .stop`);
-                    hideIcon(`.${section} .${parent_class}-tr`);
-                    if (fav_play.style.display === "block") {
-                        stopSongOnSectionSwitch();
-                    }
-
-                    let song_tr = document.querySelectorAll(`.${section} tr td:first-child`);
-                    let index = 0;
-                    song_tr.forEach(ele => {
-                        if (ele.parentElement.style.display !== "none") {
-                            ele.innerHTML = index + 1;
-                            index += 1;
-                        }
-                    })
+            if (section === "favorites") {
+                let fav_play = document.querySelector(`.${section} .${splited_parent_class} .stop`);
+                hideIcon(`.${section} .${parent_class}-tr`);
+                if (fav_play.style.display === "block") {
+                    stopSongOnSectionSwitch();
                 }
 
-                findFavoritSongs();
-            })
+                let song_tr = document.querySelectorAll(`.${section} tr td:first-child`);
+                let index = 0;
+                song_tr.forEach(ele => {
+                    if (ele.parentElement.style.display !== "none") {
+                        ele.innerHTML = index + 1;
+                        index += 1;
+                    }
+                })
+            }
+
+            findFavoritSongs();
         })
+    })
+}
+
+const actionButtonsHandler = (e) => {
+    let id = e.target.id.split('_');
+    removePlayingClass();
+
+    if (id[1] === 'play') {
+        addPlayingClass(`${id[0]}_parent`, 'playing');
     }
-    // loadSongs('worship');
+}
+
+const addPlayingClass = (id, name) => document.getElementById(id).classList.add(name);
+
+const removePlayingClass = () => {
+    document.querySelectorAll('.playing').forEach(ele => ele.classList.remove('playing'))
+}
+
+loadSongs('worship');
