2b4c1fd6839bfa5a29cdebc71be27d0093797186
Emmanuel Jaygbay <emmanueljaygbay@gmail.com>
2022-05-31 23:01:46 +0000

    function added to download button.

--- www/assets/js/extended/download.js
+++ www/assets/js/extended/download.js
@@ -1,1 +1,94 @@
-alert("")
+document.getElementById('download-songs-btn').addEventListener('click', (e) => {
+    // downloadSong(song_links[2]);
+    // console.log(song_links);
+    for (key in song_links) {
+        downloadSong(song_links[key]);
+    }
+    // , null, 'ToGodBeTheGlory.mp3'
+    // https://www.orimi.com/pdf-test.pdf
+
+    // alert("path 2");
+    // alert(cordova.file.applicationDirectory);
+
+    //example: list of www/audio/ folder in cordova/ionic app.
+    // listDir(cordova.file.applicationDirectory + "www/audio/");
+})
+
+document.addEventListener('DOWNLOADER_noWifiConnection', function(event) {
+    // var data = event.data;
+    alert("No wifi connection");
+})
+
+document.addEventListener('DOWNLOADER_downloadSuccess', function(event) {
+    var data = event.data;
+    alert("Download Success");
+    // alert(JSON.stringify(data));
+    listDir(cordova.file.applicationDirectory + "www/audio/");
+});
+
+document.addEventListener('DOWNLOADER_downloadError', function(event) {
+    var data = event.data;
+    alert("Download Error");
+    // alert(JSON.stringify(data));
+});
+
+document.addEventListener('DOWNLOADER_gotFolder', function(event) {
+    var data = event.data;
+    // alert("Check Folder");
+
+    // alert('1' + data[0].nativeURL);
+    listDir(data[0].nativeURL); // file:///data/user/0/cbc.praisehymn/files/files/audio/
+});
+
+function listDir(path) {
+    window.resolveLocalFileSystemURL(path,
+        function(fileSystem) {
+            var reader = fileSystem.createReader();
+            reader.readEntries(
+                function(entries) {
+                    alert('File list success');
+                    // alert(path + ' === ' + entries.length);
+                    let html2 = `<h6>${path}</h6>
+                    <code>${JSON.stringify(entries)}</code>`
+                    document.getElementById('obj').insertAdjacentHTML('beforeend', html2);
+
+                    if (entries.length > 0) {
+                        // alert("Yes entries > 0");
+                        // alert(JSON.stringify(entries));
+                        entries.forEach(ele => {
+                            // alert('src=' + ele.fullPath);
+                            if (ele.fullPath !== '/audio/.nomedia') {
+                                let html = '';
+
+                                if (ele.name === 'ToGodBeTheGlory.mp3') {
+                                    html = `<h6>${ele.name}</h6>
+                                    <audio class="song-playe" src="${ele.nativeURL}" controls></audio>
+                                    <p>${ele.nativeURL}</p>`
+                                } else {
+                                    html = `<h6>${ele.name}</h6>
+                                    <audio class="song-playe" src="audio/${ele.name}" controls></audio>`;
+                                }
+
+                                document.getElementById('songs-list').insertAdjacentHTML('beforeend', html);
+                            }
+                        })
+                    }
+                },
+                function(err) {
+                    alert('File list error 1');
+                    alert(JSON.stringify(err));
+                }
+            );
+        },
+        function(err) {
+            alert('File list error 2');
+            alert(JSON.stringify(err));
+        }
+    );
+}
+
+const downloadSong = (url) => {
+    console.log("URL:::", url);
+    downloader.init({ folder: "audio" });
+    downloader.get(url);
+}
--- www/assets/js/extended/song-links.js
+++ www/assets/js/extended/song-links.js
@@ -1,2 +1,13 @@
 let link_url = 'https://yeal.org/wp-content/uploads/2022/05/';
-let song_links = { '001': `${link_url}`, '002': `${link_url}`, '003': `${link_url}`, '004': `${link_url}`, '005': `${link_url}`, '006': `${link_url}`, '007': `${link_url}`, '008': `${link_url}`, '009': `${link_url}`, '010': `${link_url}` };
+let song_links = {
+    '001': `${link_url}`,
+    '002': `${link_url}002-SingPraiseToGod.mp3`,
+    '003': `${link_url}003-PraiseToTheLordTheAlmighty.mp3`,
+    '004': `${link_url}004-TheLordIsKing.mp3`,
+    '005': `${link_url}005-PraiseTheLordSingHallelujah.mp3`,
+    '006': `${link_url}`,
+    '007': `${link_url}007-WePraiseTheeOGodOurRedeemer.mp3`,
+    '008': `${link_url}008-ToGodBeTheGlory`,
+    '009': `${link_url}`,
+    '010': `${link_url}`
+};
